28aa4931bef2d76d67605767752ed9b4ece93aea
Carlos Alberto Bandeira Filho <80410556+carlosabfi@users.noreply.github.com>
2022-10-31 21:13:30 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import NameForm from './Form2';
+import reportWebVitals from './reportWebVitals';
+import Form from './Form3';
+import Form4 from './Form4';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+  
+   <Form4 />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
